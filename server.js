de2c8b9f815f8fca96ce073908ca938232ef0357
Rushil Kumar <rukumar333@gmail.com>
2014-11-02 00:24:09 +0100

    more testing with sendgrid

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.get('/', function(req, res){
     res.send('Hello World!');
 });
 
+/*
 app.post('/',function(req, res){
     var emailNumber = req.body.email;
     res.writeHead(200, {"Content-Type": "text/plain"});
@@ -42,9 +43,10 @@ app.post('/',function(req, res){
     console.log("This is the emailNumber stored: " + contact.email);
     res.end();
 });
-
+*/
 app.post('/email',function(req, res){
     var emailNumber = req.body.from;
+    console.log("This is the BODY!!!: \n" + req.body);
     res.writeHead(200, {"Content-Type": "text/plain"});
     console.log("This is the phone number " + emailNumber);
     var contact = new Contacts({email:emailNumber});
