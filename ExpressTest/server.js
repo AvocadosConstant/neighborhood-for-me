85e23237a4cc12393a4e3271fa211068245d7806
Rushil Kumar <rukumar333@gmail.com>
2014-11-02 00:16:21 +0100

    testing server.js

--- ExpressTest/server.js
+++ ExpressTest/server.js
@@ -43,6 +43,18 @@ app.post('/',function(req, res){
     res.end();
 });
 
+app.post('/email',function(req, res){
+    var emailNumber = req.body.from;
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    console.log("This is the phone number " + emailNumber);
+    var contact = new Contacts({email:emailNumber});
+    contact.save(function(err, contact){
+        if(err) return console.log(err);
+    });
+    console.log("This is the emailNumber stored: " + contact.email);
+    res.end();
+});
+
 var server = app.listen(3000, function(){
     var host = server.address().address;
     var port = server.address().port;
