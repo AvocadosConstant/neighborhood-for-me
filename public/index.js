5e839051a2bd2c957fe5e8f0f39de067f9b75cb4
Tim Hung <thung1@binghamton.edu>
2014-11-02 04:49:13 +0000

    Created all sidebar pages for the site. Wrote jqueries to link to different pages! :D

---
+++ public/index.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+    $('#elem1').hover(
+        function() { $('#elem1').css('background-color', 'grey'); },
+        function() { $('#elem1').css('background-color', 'black'); }
+    );
+    $('#elem1').click(
+        function() { document.location.href = "file:///C:/Users/Tim/Documents/GitHubRepositories/CodingForACauseSocialHackathon/public/home.html";}
+    );
+});
+
+$(document).ready(function() {
+    $('#elem2').hover(
+        function() { $('#elem2').css('background-color', 'grey'); },
+        function() { $('#elem2').css('background-color', 'black'); }
+    );
+    $('#elem2').click(
+        function() { document.location.href = "file:///C:/Users/Tim/Documents/GitHubRepositories/CodingForACauseSocialHackathon/public/signupin.html";}
+    );
+});
+
+$(document).ready(function() {
+    $('#elem3').hover(
+        function() { $('#elem3').css('background-color', 'grey'); },
+        function() { $('#elem3').css('background-color', 'black'); }
+    );
+    $('#elem3').click(
+        function() { document.location.href = "file:///C:/Users/Tim/Documents/GitHubRepositories/CodingForACauseSocialHackathon/public/connect.html";}
+    );
+});
+
+$(document).ready(function() {
+    $('#elem4').hover(
+        function() { $('#elem4').css('background-color', 'grey'); },
+        function() { $('#elem4').css('background-color', 'black'); }
+    );
+    $('#elem4').click(
+        function() { document.location.href = "file:///C:/Users/Tim/Documents/GitHubRepositories/CodingForACauseSocialHackathon/public/orgs.html";}
+    );
+});
+
+$(document).ready(function() {
+    $('#elem5').hover(
+        function() { $('#elem5').css('background-color', 'grey'); },
+        function() { $('#elem5').css('background-color', 'black'); }
+    );
+    $('#elem5').click(
+        function() { document.location.href = "file:///C:/Users/Tim/Documents/GitHubRepositories/CodingForACauseSocialHackathon/public/pricig.html";}
+    );
+});
+
+$(document).ready(function() {
+    $('#elem6').hover(
+        function() { $('#elem6').css('background-color', 'grey'); },
+        function() { $('#elem6').css('background-color', 'black'); }
+    );
+    $('#elem6').click(
+        function() { document.location.href = "file:///C:/Users/Tim/Documents/GitHubRepositories/CodingForACauseSocialHackathon/public/sponsors.html";}
+    );
+});
+
+$(document).ready(function() {
+    $('#elem7').hover(
+        function() { $('#elem7').css('background-color', 'grey'); },
+        function() { $('#elem7').css('background-color', 'black'); }
+    );
+    $('#elem7').click(
+        function() { document.location.href = "file:///C:/Users/Tim/Documents/GitHubRepositories/CodingForACauseSocialHackathon/public/aboutus.html";}
+    );
+});
+
+$(document).ready(function() {
+    $('#elem8').hover(
+        function() { $('#elem8').css('background-color', 'grey'); },
+        function() { $('#elem8').css('background-color', 'black'); }
+    );
+    $('#elem8').click(
+        function() { document.location.href = "file:///C:/Users/Tim/Documents/GitHubRepositories/CodingForACauseSocialHackathon/public/support.html";}
+    );
+});
